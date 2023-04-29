39fdc6880b73b6cf0553a1906f9ae29c1198560c
Sebastian Tiger <sebastian.tiger@nexergroup.com>
2023-04-29 12:51:49 +0200

    Updates

---
+++ precache-manifest.fbf343e6dbc59f0261a8ce4e04c6b193.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd4d23fe2c64bcbb8026e589df9c947",
+    "url": "/Cv/index.html"
+  },
+  {
+    "revision": "9bd1b88f8d2a8367c445",
+    "url": "/Cv/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "58cf50719a80133ed047",
+    "url": "/Cv/static/css/main.18772993.chunk.css"
+  },
+  {
+    "revision": "9bd1b88f8d2a8367c445",
+    "url": "/Cv/static/js/2.70356de6.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/Cv/static/js/2.70356de6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58cf50719a80133ed047",
+    "url": "/Cv/static/js/main.a9686d59.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Cv/static/js/main.a9686d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5620dfb721d7d8f3f3",
+    "url": "/Cv/static/js/runtime-main.283e24fa.js"
+  },
+  {
+    "revision": "43a897fc3db524b063a0eb8bff3d8f5a",
+    "url": "/Cv/static/media/natureImg3.43a897fc.jpg"
+  },
+  {
+    "revision": "e2f274933378fc50c8e21c004cf56b10",
+    "url": "/Cv/static/media/pictureOfMe.e2f27493.jpg"
+  }
+]);
