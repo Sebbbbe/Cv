acb8de46bae5a73a3d5f90f02e284f8b3c94111e
Sebastian Tiger <sebastian.tiger@nexergroup.com>
2023-04-29 13:24:59 +0200

    ...

--- MittCv/ClientApp/src/components/FrontPage.js
+++ MittCv/ClientApp/src/components/FrontPage.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { FiArrowDown } from 'react-icons/fi';
 import  frontImage from '../Image/natureImg3.jpg';
 export class FrontPage extends Component {
-  
+ 
   constructor(){
     super();
   }
   
- 
+
 
   render() {
       return(
@@ -20,22 +20,22 @@ export class FrontPage extends Component {
      <div class="collapse navbar-collapse" id="navbarNav">
        <ul class="navbar-nav">
          <li class="nav-item active ">
-           <a class="nav-link" href="#link-about-me" >About Me <span class="sr-only">(current)</span></a>
+           <a class="nav-link text-white hoverEffect" href="#link-about-me" >About Me <span class="sr-only">(current)</span></a>
          </li>
          <li class="nav-item">
-           <a class="nav-link" href="#link-kunskaper">Kunskaper</a>
+           <a class="nav-link text-white hoverEffect" href="#link-kunskaper">Kunskaper</a>
          </li>
          <li class="nav-item">
-           <a class="nav-link" href="#link-kurser">Kurser</a>
+           <a class="nav-link text-white hoverEffect" href="#link-kurser">Kurser</a>
          </li>
          <li class="nav-item">
-           <a class="nav-link " href="#link-projekt">Projekt</a>
+           <a class="nav-link text-white hoverEffect " href="#link-projekt">Projekt</a>
          </li>
          <li class="nav-item">
-           <a class="nav-link " href="#link-lia">LIA</a>
+           <a class="nav-link text-white hoverEffect" href="#link-lia">LIA</a>
          </li>
          <li class="nav-item">
-           <a class="nav-link " href="#link-arbete">Arbete</a>
+           <a class="nav-link text-white hoverEffect" href="#link-arbete">Arbete</a>
          </li>
        </ul>
      </div>
